a321b3bc9d046f4b82eb2b51b39d84dfd7f0eac6
kkome <exazunyan1@gmail.com>
2024-05-27 17:23:21 +0900

    24/05/27 kkome

--- src/main/java/kr/communityserver/controller/UserController.java
+++ src/main/java/kr/communityserver/controller/UserController.java
@@ -1,6 +1,7 @@
 package kr.communityserver.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import kr.communityserver.dto.UserDTO;
 import kr.communityserver.entity.User;
 import kr.communityserver.security.MyUserDetails;
@@ -13,9 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -83,6 +82,26 @@ public class UserController {
 
         String uid = userService.register(userDTO);
         return Map.of("userid", uid);
+    }
+
+    @ResponseBody
+    @GetMapping("/checkEmail/{value}")
+    public ResponseEntity<?> checkEmail(HttpSession session, @PathVariable("value") String value){
+
+        boolean isExist = userService.existsByEmail(value);
+        log.info("isExist : " + isExist);
+
+        // 중복 없으면 이메일 인증코드 발송
+        if(!isExist){
+            log.info("email : " + value);
+            userService.sendEmailCode(session, value);
+        }
+
+        // Json 생성
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("result", isExist);
+
+        return ResponseEntity.ok().body(resultMap);
     }
 
 }
--- src/main/java/kr/communityserver/service/UserService.java
+++ src/main/java/kr/communityserver/service/UserService.java
@@ -1,13 +1,24 @@
 package kr.communityserver.service;
 
+import jakarta.mail.Message;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpSession;
 import kr.communityserver.dto.UserDTO;
 import kr.communityserver.entity.User;
 import kr.communityserver.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -15,6 +26,7 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final JavaMailSender javaMailSender;
 
     public String register(UserDTO userDTO) {
         String encoded = passwordEncoder.encode(userDTO.getPass());
@@ -26,4 +38,44 @@ public class UserService {
         return savedUser.getUid();
     }
 
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public void sendEmailCode(HttpSession session, String receiver){
+
+        log.info("sender : " + sender);
+
+        // MimeMessage 생성
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        // 인증코드 생성 후 세션 저장
+        String code = String.valueOf(ThreadLocalRandom.current().nextInt(100000, 1000000));
+        session.setAttribute("code", code);
+
+        log.info("code : " + code);
+
+        String title = "인증코드 입니다.";
+        String content = "<h1>인증코드는 " + code + "입니다.</h1>";
+
+
+        try {
+            message.setSubject(title);
+            message.setFrom(new InternetAddress(sender, "보내는 사람", "UTF-8"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+            message.setSubject(title);
+            message.setContent(content, "text/html;charset=UTF-8");
+
+            javaMailSender.send(message);
+
+        } catch (Exception e) {
+            log.error("error={}", e.getMessage());
+        }
+
+    }
 }
+
+
