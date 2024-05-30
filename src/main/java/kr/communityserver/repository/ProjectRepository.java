dd1efe109241768d2e2966d48abb6dcec407380e
jun9708 <wnsgud1997@gmail.com>
2024-05-30 17:31:49 +0900

    project 등록 및 페이지네이션 기능작성

--- src/main/java/kr/communityserver/repository/ProjectRepository.java
+++ src/main/java/kr/communityserver/repository/ProjectRepository.java
@@ -1,7 +1,12 @@
 package kr.communityserver.repository;
 
+import kr.communityserver.entity.Board;
 import kr.communityserver.entity.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
 }
