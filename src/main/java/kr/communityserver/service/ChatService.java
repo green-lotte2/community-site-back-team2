c29c1684b2ed9d8dd0311716af8a6758ccb65dea
dpsk08270 <dpsk08270@gmail.com>
2024-06-04 17:57:14 +0900

    게시판 신고하기 구현중

--- src/main/java/kr/communityserver/service/ChatService.java
+++ src/main/java/kr/communityserver/service/ChatService.java
@@ -5,7 +5,7 @@ import jakarta.annotation.Resource;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import kr.communityserver.DTO.UserDTO;
+import kr.communityserver.dto.UserDTO;
 import kr.communityserver.entity.*;
 import kr.communityserver.repository.*;
 import lombok.RequiredArgsConstructor;
